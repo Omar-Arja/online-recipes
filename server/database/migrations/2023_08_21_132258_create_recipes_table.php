efcf69e7ef6d9cbc08af1ef675a3190bb67ba387
Omar-Arja <omaralarja17@gmail.com>
2023-08-21 16:59:12 +0300

    Server <> created migrations

---
+++ server/database/migrations/2023_08_21_132258_create_recipes_table.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('cuisine_id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('recipe_ingredients', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('recipe_id');
+            $table->unsignedBigInteger('ingredient_id');
+            $table->string('amount');
+            $table->timestamps();
+        });
+        
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('recipe_id');
+            $table->string('image_url');
+            $table->timestamps();
+        });
+        
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('cuisines', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+
+        Schema::table('recipes', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('cuisine_id')->references('id')->on('cuisines')->onDelete('cascade');
+        });
+
+        Schema::table('recipe_ingredients', function (Blueprint $table) {
+            $table->foreign('recipe_id')->references('id')->on('recipes')->onDelete('cascade');
+            $table->foreign('ingredient_id')->references('id')->on('ingredients')->onDelete('cascade');
+        });
+
+        Schema::table('images', function (Blueprint $table) {
+            $table->foreign('recipe_id')->references('id')->on('recipes')->onDelete('cascade');
+        });
+
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+        Schema::dropIfExists('recipe_ingredients');
+        Schema::dropIfExists('images');
+        Schema::dropIfExists('cuisines');
+        Schema::dropIfExists('ingredients');
+    }
+};
---
+++ server/database/migrations/2023_08_21_134006_create_shopping_lists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('shopping_lists', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::create('shopping_list_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('shopping_list_id');
+            $table->unsignedBigInteger('recipe_id');
+            $table->timestamps();
+
+            $table->foreign('shopping_list_id')->references('id')->on('shopping_lists')->onDelete('cascade');
+            $table->foreign('recipe_id')->references('id')->on('recipes')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('shopping_lists');
+        Schema::dropIfExists('shopping_list_items');
+    }
+};
